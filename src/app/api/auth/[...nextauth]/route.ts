1f91d567d1875d3bf7fdff3488ed0c42c0523019
MUHAMMED ZULFIKER A <54462409+M-zulfiker-A@users.noreply.github.com>
2025-02-19 10:58:26 +0530

    add basic authentication

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      console.log(url);
+      return url;
+    },
+    async session({ session, user, token }) {
+      return session;
+    },
+    async jwt({ token, user, account, profile }) {
+      return token;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
